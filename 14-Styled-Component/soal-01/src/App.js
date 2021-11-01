5bb8e170b28b6eeb83f380e125d590543dba7a8c
rizkyprayatman <r.prayatman@gmail.com>
2021-11-02 01:36:53 +0800

    update task styled component

--- 14-Styled-Component/soal-01/src/App.js
+++ 14-Styled-Component/soal-01/src/App.js
@@ -1,3 +1,9 @@
+import Card from "./components/Card";
+
 export default function App() {
-  return <h1>Soal - 01</h1>;
+  return (
+    <>
+      <Card />
+    </>
+  );
 }
---
+++ 14-Styled-Component/soal-01/src/components/Button.js
@@ -0,0 +1,5 @@
+import { StyledButton } from "./styles/Button";
+
+export default function Button() {
+  return <StyledButton>Buy</StyledButton>;
+}
